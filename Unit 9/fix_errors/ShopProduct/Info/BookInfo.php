343221d3d3d0b3407068ed532ecadfb09d3bf85e
d.vyskorka <52822065+VyskorkaD@users.noreply.github.com>
2022-02-09 12:37:48 +0100

    Unit 9 folder added. Starting work.

---
+++ Unit 9/fix_errors/ShopProduct/Info/BookInfo.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+class BookInfo extends BaseInfo
+{
+    /**
+     * @inheritDoc
+     */
+    protected function getAdditionalInfo(): string
+    {
+        return "кількість сторінок: {$this->shopProduct->getPagesCount()}";
+    }
+}
---
+++ Unit 9/fix_errors/ShopProduct/Info/PhoneInfo.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+
+class PhoneInfo extends BaseInfo
+{
+    /**
+     * @inheritDoc
+     */
+    protected function getAdditionalInfo(): string
+    {
+        return "вбудована пам'ять: {$this->shopProduct->getMemory()}";
+    }
+}
